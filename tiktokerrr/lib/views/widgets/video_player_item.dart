f8dd59dcd0f6fa3c5b8b9e1bd27bfc94fac4e9b6
jinheng0301 <95295108+jinheng0301@users.noreply.github.com>
2024-03-03 15:32:51 +0800

    add looping and play/pause functions in the project

--- tiktokerrr/lib/views/widgets/video_player_item.dart
+++ tiktokerrr/lib/views/widgets/video_player_item.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -11,6 +12,9 @@ class VideoPlayerItem extends StatefulWidget {
 
 class _VideoPlayerItemState extends State<VideoPlayerItem> {
   late VideoPlayerController _videoPlayerController;
+  late bool isVideoPlaying = false;
+  late bool showControls = true;
+  late Timer controlsTimer;
 
   @override
   void initState() {
@@ -19,17 +23,64 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
     _videoPlayerController = VideoPlayerController.networkUrl(
       Uri.parse(widget.videoUrl),
       // convert String to Uri
-    )..initialize().then(
-        (value) => {
-          _videoPlayerController.play(),
-          _videoPlayerController.setVolume(1),
-        },
-      );
+    )..initialize().then((_) {
+        setState(() {
+          _videoPlayerController.setLooping(true);
+          _videoPlayerController.play();
+          _videoPlayerController.setVolume(1);
+          isVideoPlaying = true;
+        });
+      });
+
+    controlsTimer = Timer.periodic(
+      Duration(seconds: 2),
+      (timer) {
+        if (!isVideoPlaying) {
+          timer.cancel();
+          // stop the timer if the video is paused
+        }
+        setState(() {
+          showControls = false;
+          // Hide controls after a few seconds
+        });
+      },
+    );
+  }
+
+  void _togglePlayPause() {
+    if (isVideoPlaying) {
+      _videoPlayerController.pause();
+    } else {
+      _videoPlayerController.play();
+    }
+    setState(() {
+      isVideoPlaying = !isVideoPlaying;
+      showControls = true;
+      // Show controls when toggling play/pause
+    });
+
+    // Restart the timer when controls are shown
+    controlsTimer.cancel();
+
+    controlsTimer = Timer.periodic(
+      Duration(seconds: 3),
+      (timer) {
+        if (!isVideoPlaying) {
+          timer.cancel();
+        }
+        setState(() {
+          showControls = false;
+        });
+      },
+    );
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    _videoPlayerController.dispose();
+    controlsTimer.cancel();
+    // cancel the timer to avoid memory leaks
     super.dispose();
   }
 
@@ -37,13 +88,33 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
 
-    return Container(
-      height: size.height,
-      width: size.width,
-      decoration: BoxDecoration(
-        color: Colors.black,
-      ),
-      child: VideoPlayer(_videoPlayerController),
+    return Stack(
+      children: [
+        GestureDetector(
+          onTap: _togglePlayPause,
+          child: Container(
+            height: size.height,
+            width: size.width,
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: VideoPlayer(_videoPlayerController),
+          ),
+        ),
+        Visibility(
+          visible: showControls,
+          child: Center(
+            child: IconButton(
+              onPressed: _togglePlayPause,
+              icon: Icon(
+                isVideoPlaying ? Icons.pause : Icons.play_arrow,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
