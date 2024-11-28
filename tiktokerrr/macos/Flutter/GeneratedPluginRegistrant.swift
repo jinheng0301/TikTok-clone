c0bd094e928f94c93f6ba25df49da585c787ec6d
jinheng0301 <95295108+jinheng0301@users.noreply.github.com>
2024-11-29 00:53:36 +0800

    update the dependencies to the latest versions.

--- tiktokerrr/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tiktokerrr/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 import video_compress
 import video_player_avfoundation
 
